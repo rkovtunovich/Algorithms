ef443e065413ff96122f0da95359720f565cecc0
bliver <bliver@ukr.net>
2023-05-03 13:25:33 +0300

    added bubble sort

---
+++ Sorting/BubbleSort.cs
@@ -0,0 +1,24 @@
+﻿namespace Sorting;
+
+public static class BubbleSort
+{
+    public static void Sort<T>(T[] array) where T : IComparable<T>
+    { 
+        int n = array.Length;
+        bool swapped;
+
+        do
+        {
+            swapped = false;
+            for (int i = 1; i < n; i++)
+            {
+                if (array[i - 1].CompareTo(array[i]) > 0)
+                {
+                    (array[i], array[i - 1]) = (array[i - 1], array[i]);
+                    swapped = true;
+                }
+            }
+            n--;
+        } while (swapped);
+    }
+}

edcd4ad30b57f5f90ca52798e5cb862224bd866b
rkovtunovich <roma170586@gmail.com>
2023-10-06 12:35:02 +0300

    added finding pair with sum

---
+++ Finding/BinarySearch.cs
@@ -0,0 +1,33 @@
+﻿namespace Finding;
+
+public static class BinarySearch
+{
+    // Searches an integer array for a specified value using the binary search algorithm
+    public static int Search(int[] array, int value)
+    {
+        // Set the starting and ending indices of the search range
+        int startIndex = 0;
+        int endIndex = array.Length - 1;
+
+        // While the starting index is less than or equal to the ending index
+        while (startIndex <= endIndex)
+        {
+            // Find the middle index of the search range
+            int middleIndex = (startIndex + endIndex) / 2;
+
+            // If the value is found at the middle index, return the middle index
+            if (array[middleIndex] == value)
+                return middleIndex;
+
+            // If the value is less than the value at the middle index, search the left half of the search range
+            if (value < array[middleIndex])
+                endIndex = middleIndex - 1;
+            // If the value is greater than the value at the middle index, search the right half of the search range
+            else
+                startIndex = middleIndex + 1;
+        }
+
+        // Return -1 if the value is not found
+        return -1;
+    }
+}
---
+++ Finding/FindingPairWithSum.cs
@@ -0,0 +1,65 @@
+﻿using Finding;
+using Sorting.Common;
+
+namespace MathAlgo;
+
+public class FindingPairWithSum
+{
+    // Finds a pair of integers in an integer array that sum to a specified value
+    // using the binary search algorithm
+    public static int[]? FindPairBinarySearch(int[] array, int sum)
+    {
+        // Sort the array
+        QuickSort.Sort(array);
+
+        // Iterate through each element in the array
+        for (int i = 0; i < array.Length; i++)
+        {
+            // Find the difference between the current element and the sum
+            int difference = sum - array[i];
+
+            // Find the index of the difference in the array
+            int index = BinarySearch.Search(array, difference);
+
+            // If the difference is found and the index is not the same as the current index, return the pair
+            if (index != -1 && index != i)
+                return new int[] { array[i], array[index] };
+        }
+
+        // Return null if no pair is found
+        return null;
+    }
+
+    // Finds a pair of integers in an integer array that sum to a specified value
+    // using the two-pointer algorithm
+    public static int[]? FindPairTwoPointer(int[] array, int sum)
+    {
+        // Sort the array
+        QuickSort.Sort(array);
+
+        // Set the starting and ending indices of the search range
+        int startIndex = 0;
+        int endIndex = array.Length - 1;
+
+        // While the starting index is less than the ending index
+        while (startIndex < endIndex)
+        {
+            // Find the sum of the elements at the starting and ending indices
+            int currSum = array[startIndex] + array[endIndex];
+
+            // If the sum is equal to the specified sum, return the pair
+            if (currSum == sum)
+                return [array[startIndex], array[endIndex]];
+
+            // If the sum is less than the specified sum, increment the starting index
+            if (currSum < sum)
+                startIndex++;
+            // If the sum is greater than the specified sum, decrement the ending index
+            else
+                endIndex--;
+        }
+
+        // Return null if no pair is found
+        return null;
+    }
+}

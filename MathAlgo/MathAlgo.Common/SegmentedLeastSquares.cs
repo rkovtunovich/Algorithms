20b969e87fcd31e111a9d39d5d6d529b78b8b3da
rkovtunovich <roma170586@gmail.com>
2024-08-16 09:29:54 +0300

    .

---
+++ MathAlgo/MathAlgo.Common/SegmentedLeastSquares.cs
@@ -0,0 +1,77 @@
+﻿using Spacing.Core;
+using Spacing.Core.Points;
+
+namespace MathAlgo.Common;
+
+public class SegmentedLeastSquares
+{
+    public static List<Line> Calculate(List<Point2D> points, double penalty = 1.0)
+    {
+        // sort points by x
+        points.Sort((a, b) => a.X.CompareTo(b.X));
+
+        var lines = new List<Line>();
+        var n = points.Count;
+        var dp = new double[n];
+        var prev = new int[n];
+        dp[0] = 0;
+        prev[0] = -1;
+
+        for (var i = 1; i < n; i++)
+        {
+            dp[i] = double.MaxValue;
+            for (var j = 0; j < i; j++)
+            {
+                var cost = dp[j] + Cost(points, j, i);
+                if (cost < dp[i])
+                {
+                    dp[i] = cost;
+                    prev[i] = j;
+                }
+            }
+        }
+
+        for (var i = n - 1; i != -1; i = prev[i])
+        {
+            var line = FitLine(points, prev[i], i);
+            lines.Add(line);
+        }
+
+        lines.Reverse();
+        return lines;
+    }
+
+    private static Line FitLine(List<Point2D> points, int v, int i)
+    {
+        var n = i - v + 1;
+        var sumX = 0.0;
+        var sumY = 0.0;
+        var sumXY = 0.0;
+        var sumX2 = 0.0;
+        for (var j = v; j <= i; j++)
+        {
+            sumX += points[j].X;
+            sumY += points[j].Y;
+            sumXY += points[j].X * points[j].Y;
+            sumX2 += points[j].X * points[j].X;
+        }
+
+        var slope = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX);
+        var yIntercept = (sumY - slope * sumX) / n;
+
+        return new Line(slope, yIntercept);
+    }
+
+    private static double Cost(List<Point2D> points, int j, int i)
+    {
+        var line = FitLine(points, j, i);
+        var cost = 0.0;
+        for (var k = j; k <= i; k++)
+        {
+            var y = line.GetY(points[k].X);
+            cost += Math.Pow(y - points[k].Y, 2);
+        }
+
+        return cost;
+    }
+}
---
+++ MathAlgo/MathAlgo.Tests/Common/SegmentedLeastSquaresShould.cs
@@ -0,0 +1,45 @@
+﻿using Spacing.Core.Points;
+
+namespace MathAlgo.Tests.Common;
+public class SegmentedLeastSquaresShould
+{
+    [Fact]
+    public void Calculate_WhenTwoPoints_ReturnsCorrectLines()
+    {
+        var points = new List<Point2D>
+        {
+            new(0, 0),
+            new(1, 1),
+        };
+
+        var lines = SegmentedLeastSquares.Calculate(points);
+
+        lines.Should().ContainSingle();
+        lines[0].Slope.Should().BeApproximately(1, 1e-6);
+        lines[0].YIntercept.Should().BeApproximately(0, 1e-6);
+    }
+
+    [Fact]
+    public void Calculate_ReturnsCorrectLines()
+    {
+        var points = new List<Point2D>
+        {
+            new Point2D(0, 0),
+            new Point2D(1, 1),
+            new Point2D(2, 2),
+            new Point2D(3, 3),
+            new Point2D(4, 4),
+            new Point2D(5, 5),
+            new Point2D(6, 6),
+            new Point2D(7, 7),
+            new Point2D(8, 8),
+            new Point2D(9, 9),
+        };
+
+        var lines = SegmentedLeastSquares.Calculate(points);
+
+        lines.Should().ContainSingle();
+        lines[0].Slope.Should().BeApproximately(1, 1e-6);
+        lines[0].YIntercept.Should().BeApproximately(0, 1e-6);
+    }
+}
